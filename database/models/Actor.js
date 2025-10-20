7ba43cc5fafa40cc8829b3c281a386f75801f6c1
bautistagorchs <bautista.gorchs@gmail.com>
2025-10-20 17:52:30 -0300

    crregir actor.js

--- database/models/Actor.js
+++ database/models/Actor.js
@@ -1,53 +1,38 @@
-// database/models/Actor.js
-module.exports = (sequelize, DataTypes) => {
-  const Actor = sequelize.define(
-    'Actor',
-    {
-      id: {
-        type: DataTypes.INTEGER.UNSIGNED,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
-      },
-      first_name: {
-        type: DataTypes.STRING(100), 
-        allowNull: false,
-      },
-      last_name: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-      },
-      rating: {
-        type: DataTypes.DECIMAL(3, 1), 
-        allowNull: true,
-      },
-      favorite_movie_id: {
-        type: DataTypes.INTEGER.UNSIGNED, 
-        allowNull: true,
-        references: { model: 'movies', key: 'id' },
-      },
+module.exports = function (sequelize, DataTypes) {
+  let alias = "Actor";
+
+  let columns = {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
     },
-    {
-      tableName: 'actors',
-      timestamps: true,
-      underscored: true,
-    }
-  );
-
-  Actor.associate = (models) => {
-    Actor.belongsToMany(models.Movie, {
-      as: 'movies',
-      through: 'actor_movie',
-      foreignKey: 'actor_id',
-      otherKey: 'movie_id',
-      timestamps: true,
-    });
-
-    Actor.belongsTo(models.Movie, {
-      as: 'favoriteMovie',
-      foreignKey: 'favorite_movie_id',
-    });
+    first_name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.DECIMAL(3, 1),
+      allowNull: true,
+    },
+    favorite_movie_id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: true,
+      references: { model: "movies", key: "id" },
+    },
   };
 
+  let config = {
+    tableName: "actors",
+    timestamps: true,
+    underscored: true,
+  };
+
+  const Actor = sequelize.define(alias, columns, config);
   return Actor;
 };
